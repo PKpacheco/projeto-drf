f30af376e081ea83709078380bf9b16a66616230
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2017-01-10 19:32:32 -0300

    ajustando views.py

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 from django.conf.urls import url
 from . import DefaultRouter
-from portfolios.views import PortfolioViewSet
+from portfolios.views import PortfolioViewSet, PortfolioListViewSet
 
 
 router = DefaultRouter()
 
 helper_patterns = [
-    url(r'^portfolios/$', PortfolioViewSet.as_view(), name='portfolios'),
+    url(r'^portfolios/$', PortfolioListViewSet.as_view(), name='portfolios'),
+    url(r'^portfolios/(?P<pk>[0-9]+)/$', PortfolioViewSet.as_view(), name='get_portfolio')
 ]
 
 urlpatterns = helper_patterns
--- portfolios/views.py
+++ portfolios/views.py
@@ -8,8 +8,28 @@ from rest_framework.views import APIView
 
 
 class PortfolioViewSet(APIView):
+
+    def get(self, request, pk, format=None):
+        if not DadosPessoais.objects.filter(pk=pk).exists():
+            dados_pessoais = DadosPessoaisSerializer([])
+        else:
+            dados_pessoais = DadosPessoais.objects.get(pk=pk)
+            serializer = DadosPessoaisSerializer(dados_pessoais)
+
+        return Response(serializer.data)
+
+
+class PortfolioListViewSet(APIView):
     serializer_class = DadosPessoaisSerializer
 
     def get(self, request, format=None):
-        serializer = self.serializer_class(DadosPessoais.objects.all(), many=True)
+        _category = self.request.query_params.get('category', None)
+
+        if _category:
+            queryset = DadosPessoais.objects.filter(category__id=_category)
+        else:
+            queryset = DadosPessoais.objects.all()
+
+        serializer = self.serializer_class(queryset, many=True)
+
         return Response(serializer.data)
