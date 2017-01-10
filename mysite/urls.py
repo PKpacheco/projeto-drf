443ddf350a83dc9a4ba41919f4f7ed173c8f5aed
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2017-01-10 19:07:17 -0300

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('api.urls')),
     url(r'', include('portfolios.urls')),
 
 ]
--- portfolios/urls.py
+++ portfolios/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.portfolio_exibir),
 ]
