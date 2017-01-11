b18098284d88ae0a9d2b3d5ae53a8fedef04f8c6
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2017-01-11 14:21:15 -0300

    ajustes

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,18 @@
 # coding: utf-8
 from django.conf.urls import url
 from . import DefaultRouter
-from portfolios.views import PortfolioViewSet, PortfolioListViewSet
+from portfolios.views import  PortfolioViewSet, PortfolioUpdateView
 
 
 router = DefaultRouter()
 
 helper_patterns = [
-    url(r'^portfolios/$', PortfolioListViewSet.as_view(), name='portfolios'),
-    url(r'^portfolios/(?P<pk>[0-9]+)/$', PortfolioViewSet.as_view(), name='get_portfolio')
+    url(r'^portfolios/$', PortfolioViewSet.as_view(), name='portfolios'),
+    # url(r'^portfolios/(?P<pk>[0-9]+)/$', PortfolioViewSet.as_view(), name='get_portfolio'),
+    url(r'^portfolios/(?P<pk>[0-9]+)/$', PortfolioUpdateView.as_view(), name='update_portfolio')
+
 ]
+
 
 urlpatterns = helper_patterns
 urlpatterns.extend(router.urls)
--- portfolios/views.py
+++ portfolios/views.py
@@ -6,7 +6,30 @@ from .models import DadosPessoais
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_framework import status
 
+from django.shortcuts import render
+
+
+def portfolio_exibir(request):
+    pessoa = DadosPessoais.objects.all()
+    context = {'pessoa': pessoa}
+
+    return render(request, 'portfolios/portfolio_exibir.html', context)
+
+
+'''
+-- Primeiro Código --
+class PortfolioViewSet(APIView):
+    serializer_class = DadosPessoaisSerializer
+
+    def get(self, request, format=None):
+        serializer = self.serializer_class(DadosPessoais.objects.all(), many=True)
+        return Response(serializer.data)
+'''
+
+'''
+-- Segundo Código --
 class PortfolioViewSet(APIView):
 
     def get(self, request, pk, format=None):
@@ -33,3 +56,37 @@ class PortfolioListViewSet(APIView):
         serializer = self.serializer_class(queryset, many=True)
 
         return Response(serializer.data)
+'''
+
+
+class PortfolioViewSet(APIView):
+    serializer_class = DadosPessoaisSerializer
+
+    def get(self, request, format=None):
+        serializer = self.serializer_class(DadosPessoais.objects.all(), many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "403 Forbidden"}, status=status.HTTP_409_CONFLICT)
+
+
+class PortfolioUpdateView(APIView):
+
+    def get(self, request, pk, format=None):
+        user = DadosPessoais.objects.get(pk=pk)
+        serializer = DadosPessoaisSerializer(user)
+        return Response(serializer.data)
+
+    def post(self, request, pk=None):
+        user = DadosPessoais.objects.get(pk=pk)
+        serializer = DadosPessoaisSerializer(data=request.data, instance=user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
